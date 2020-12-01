048c2364448fa4e19fe2c321151659520e3f13ca
Kenneth Hoste <kenneth.hoste@ugent.be>
2020-12-01 09:02:08 +0100

    solution day 1 (second half)

--- day01.py
+++ day01.py
@@ -11,11 +11,35 @@ with open(input_file, 'r') as fp:
 
 entries = [int(x.strip()) for x in input_txt.splitlines()]
 
+solution_pair = None
 for id1, entry1 in enumerate(entries):
     for entry2 in entries[id1+1:]:
         if entry1 + entry2 == 2020:
-            print("%d * %d = %d" % (entry1, entry2, entry1 * entry2))
-            sys.exit(0)
+            solution_pair = (entry1, entry2)
+            break
+    if solution_pair:
+        break
 
-sys.stderr.write("No solution found?!\n")
-sys.exit(1)
+if solution_pair:
+    print("%d * %d = %d" % (entry1, entry2, entry1 * entry2))
+else:
+    sys.stderr.write("No solution pair found?!\n")
+    sys.exit(1)
+
+solution_triple = None
+for id1, entry1 in enumerate(entries):
+    for id2, entry2 in enumerate(entries[id1+1:]):
+        for entry3 in entries[id1+id2+2:]:
+            if entry1 + entry2 + entry3 == 2020:
+                solution_triple = (entry1, entry2, entry3)
+                break
+        if solution_triple:
+            break
+    if solution_triple:
+        break
+
+if solution_triple:
+    print("%d * %d * %d = %d" % (entry1, entry2, entry3, entry1 * entry2 * entry3))
+else:
+    sys.stderr.write("No solution triple found?!\n")
+    sys.exit(1)
