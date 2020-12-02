b71115852e4c58d6c8f74f9347afe55afe6af337
Kenneth Hoste <kenneth.hoste@ugent.be>
2020-12-02 08:27:00 +0100

    add solution for second part of day 2

--- day02/solution.py
+++ day02/solution.py
@@ -9,7 +9,8 @@ input_file = sys.argv[1]
 with open(input_file, 'r') as fp:
     input_txt = fp.read()
 
-valid_cnt = 0
+valid_cnt_part1 = 0
+parsed_entries = []
 
 for line in input_txt.splitlines():
     policy, rest = line.split(' ', 1)
@@ -18,7 +19,21 @@ for line in input_txt.splitlines():
     letter, password = rest.split(':')
     password = password.lstrip()
 
-    if min_cnt <= password.count(letter) <= max_cnt:
-        valid_cnt += 1
+    parsed_entries.append((min_cnt, max_cnt, letter, password))
 
-print(valid_cnt)
+    if min_cnt <= password.count(letter) <= max_cnt:
+        valid_cnt_part1 += 1
+
+print("solution part 1: %d" % valid_cnt_part1)
+
+#############################################################
+
+valid_cnt_part2 = 0
+
+for (idx1, idx2, letter, password) in parsed_entries:
+    if password[idx1-1] == letter and password[idx2-1] != letter:
+        valid_cnt_part2 += 1
+    if password[idx1-1] != letter and password[idx2-1] == letter:
+        valid_cnt_part2 += 1
+
+print("solution part 2: %d" % valid_cnt_part2)
