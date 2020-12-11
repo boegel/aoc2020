e48ac29601f81055eb758e40ef116e75951579c2
Kenneth Hoste <kenneth.hoste@ugent.be>
2020-12-11 23:01:16 +0100

    add solution for second half day 11

--- day11/solution.py
+++ day11/solution.py
@@ -11,7 +11,7 @@ with open(input_file, 'r') as fp:
     input_txt = fp.read()
 
 
-def apply_rules(seat_rows):
+def apply_rules_first_half(seat_rows):
     orig_seat_rows = copy.deepcopy(seat_rows)
 
     n_rows = len(seat_rows)
@@ -66,9 +66,122 @@ prev = []
 idx = 0
 while prev != seat_rows:
     prev = copy.deepcopy(seat_rows)
-    apply_rules(seat_rows)
+    apply_rules_first_half(seat_rows)
     print("\nafter pass #%d:\n" % idx)
     show(seat_rows)
     idx += 1
 
-print("first half: %d" % len([x for row in seat_rows for x in row if x == '#']))
+print("\n\nfirst half: %d" % len([x for row in seat_rows for x in row if x == '#']))
+
+print('\n' * 5 + '-' * 100 + '\n' * 5)
+
+
+def apply_rules_second_half(seat_rows):
+    orig_seat_rows = copy.deepcopy(seat_rows)
+
+    n_rows = len(seat_rows)
+    n_cols = len(seat_rows[0])
+
+    for i in range(n_rows):
+        for j in range(n_cols):
+            seat = orig_seat_rows[i][j]
+            if seat == '.':
+                continue
+            else:
+                cnt = 0
+                # up
+                ii = i - 1
+                while(ii >= 0):
+                    other_seat = orig_seat_rows[ii][j]
+                    if other_seat != '.':
+                        if other_seat == '#':
+                            cnt += 1
+                        break
+                    ii -= 1
+                # down
+                ii = i + 1
+                while(ii < n_rows):
+                    other_seat = orig_seat_rows[ii][j]
+                    if other_seat != '.':
+                        if other_seat == '#':
+                            cnt += 1
+                        break
+                    ii += 1
+                # left
+                jj = j - 1
+                while(jj >= 0):
+                    other_seat = orig_seat_rows[i][jj]
+                    if other_seat != '.':
+                        if other_seat == '#':
+                            cnt += 1
+                        break
+                    jj -= 1
+                # right
+                jj = j + 1
+                while(jj < n_cols):
+                    other_seat = orig_seat_rows[i][jj]
+                    if other_seat != '.':
+                        if other_seat == '#':
+                            cnt += 1
+                        break
+                    jj += 1
+                # top left
+                ii, jj = i - 1, j - 1
+                while(ii >= 0 and jj >= 0):
+                    other_seat = orig_seat_rows[ii][jj]
+                    if other_seat != '.':
+                        if other_seat == '#':
+                            cnt += 1
+                        break
+                    ii -= 1
+                    jj -= 1
+                # top right
+                ii, jj = i - 1, j + 1
+                while(ii >= 0 and jj < n_cols):
+                    other_seat = orig_seat_rows[ii][jj]
+                    if other_seat != '.':
+                        if other_seat == '#':
+                            cnt += 1
+                        break
+                    ii -= 1
+                    jj += 1
+                # lower left
+                ii, jj = i + 1, j - 1
+                while(ii < n_rows and jj >= 0):
+                    other_seat = orig_seat_rows[ii][jj]
+                    if other_seat != '.':
+                        if other_seat == '#':
+                            cnt += 1
+                        break
+                    ii += 1
+                    jj -= 1
+                # lower right
+                ii, jj = i + 1, j + 1
+                while(ii < n_rows and jj < n_cols):
+                    other_seat = orig_seat_rows[ii][jj]
+                    if other_seat != '.':
+                        if other_seat == '#':
+                            cnt += 1
+                        break
+                    ii += 1
+                    jj += 1
+
+                if seat == 'L' and cnt == 0:
+                    seat_rows[i][j] = '#'
+                elif seat == '#' and cnt >= 5:
+                    seat_rows[i][j] = 'L'
+
+
+seat_rows = [list(x) for x in input_txt.splitlines()]
+show(seat_rows)
+
+prev = []
+idx = 0
+while prev != seat_rows:
+    prev = copy.deepcopy(seat_rows)
+    apply_rules_second_half(seat_rows)
+    print("\nafter pass #%d:\n" % idx)
+    show(seat_rows)
+    idx += 1
+
+print("\n\nsecond half: %d" % len([x for row in seat_rows for x in row if x == '#']))
